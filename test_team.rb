ebb12beaf721d31222b3db370234e8a0d5d3f292
marmo002 <marmo_02@hotmail.com>
2017-11-15 12:54:24 -0500

    dont know how to break this code

--- test_team.rb
+++ test_team.rb
@@ -5,11 +5,11 @@ require './team.rb'
 class TestTeam < MiniTest::Test
 
   def setup
-    @barca = Team.new("FCBarcelona", "Pros", ["Messi", "Iniesta", "Pique"])
+    @barca = Team.new("FCBarcelona", "First Division", ["Messi", "Iniesta", "Pique"])
   end
 
   def test_check_hash_returned
-    assert_equal({:team_name=>"FCBarcelona", :level=>"Pros", :points=>0}, @barca.to_hash)
+    assert_equal({:team_name=>"FCBarcelona", :level=>"First Division", :points=>0}, @barca.to_hash)
   end
 
 end
